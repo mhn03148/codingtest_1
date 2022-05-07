467310c25fcad38cc77e24866f3258e7e1a19249
jujinhyeong <mhn03148@naver.com>
2022-05-07 13:59:34 +0900

    implementation

--- implementation/implementation_1.py
+++ implementation/implementation_1.py
@@ -1,3 +1,27 @@
 N = input()
 Lord = list(map(str, input().split()))
+x, y = 1,1
 
+for i in Lord:
+    if(i == 'U'):
+        if(x==1):
+            x=x
+        else:
+            x-=1
+    elif(i == 'D'):
+        if(x==N):
+            continue
+        else:
+            x+=1
+    elif (i == 'R'):
+        if (y == N):
+            continue
+        else:
+            y += 1
+    elif (i == 'L'):
+        if (y == 1):
+            continue
+        else:
+            y -= 1
+
+print(x, y)
