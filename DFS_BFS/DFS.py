ef4959522c7c63b9f8601012eaaaaa5d376953d4
jujinhyeong <mhn03148@naver.com>
2022-09-28 16:55:43 +0900

    DFS는 스택 자료구조에 기초한다는 점에서 구현이 간단하다. 실제로는 스택을 쓰지 않아도 되며 탐색을 수행함에 있어서 데이터의 개수가 N개인 경우 O(N)의 시간이 소요된다.

--- DFS_BFS/DFS.py
+++ DFS_BFS/DFS.py
@@ -2,7 +2,7 @@ def dfs(graph,v,visited):
     visited[v] = True
     print(v, end = ' ')
     for i in graph[v]:
-        if not visited[i]:
+        if not visited[i]:#visited[i]가 False일 경우
             dfs(graph, i , visited)
 
 graph = [[],
@@ -17,4 +17,11 @@ graph = [[],
 visited = [False] * 9
 
 dfs(graph,1,visited)
+
+#list에 Fasle가 있을때, if not list[]: 면 동작
+list1 = [False,False,True]
+print(list1)
+for i in range(3):
+    if not list1[i]:
+        print('hello')
 
