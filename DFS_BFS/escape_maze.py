e37a71e95e17e69b42700268a8b9dcbc47dc6579
jujinhyeong <mhn03148@naver.com>
2022-09-29 20:21:28 +0900

    N X M 크기의 직사각형 형태의 미로에 갇혀 있다. 미로에는 여러 마리의 괴물이 있어 이를 피해 탈출해야 한다. 처음 위치는(1,1)이고 미로의 출구는(N,M)의 위치에 존재하며 한번에 한 칸씩 이동할 수 있다. 이때 괴물이 있는 부분은 0으로, 괴물이 없는 부분은 1로 표시되어 있다. 미로는 반드시 탈출할 수 있는 형태로 제시된다. 이때 탈출하기 위한 최소칸의 개수를 구하여라     입력조건     •첫 번째 줄에 두 정수 N,M(4<=N,M<=200)이 주어집니다. 다음 N개의 줄에는 각각 M개의 정수(0혹은1)로 미로의 정보가 주어진다. 각각의 수들은 공백 없이 붙어서 입력으로 제시된다. 또한 시작 칸과 마지막 칸은 항상 1이다.     출력조건     •첫째 줄에 최소 이동 칸의 개수를 출력한다.

--- DFS_BFS/escape_maze.py
+++ DFS_BFS/escape_maze.py
@@ -1,1 +1,12 @@
-n,m
+from collections import deque
+n,m = map(int,input().split())
+graph = []
+for i in range(n):
+    for j in range(m):
+        graph.append(list(map(int,input())))
+
+def bfs(x, y):
+    queue = deque()
+    queue.append((x,y))
+    while queue:
+        queue.popleft()
