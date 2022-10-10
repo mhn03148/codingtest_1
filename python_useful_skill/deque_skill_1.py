e0f8debaf1840069f25fced70b11ee4b50ca0bb2
jujinhyeong <mhn03148@naver.com>
2022-10-10 16:20:43 +0900

    큐를 선언할때, 문자열을 바로 deque(문자열)을 이용하여 deque로 만들 수 있다.

--- python_useful_skill/deque_skill_1.py
+++ python_useful_skill/deque_skill_1.py
@@ -1,7 +1,49 @@
 from collections import deque
 
-s = "abbaabb"
 
-q = deque(s)
+#q = deque(s)
+'''
+j = len(q)
+while q:
+    before_q=len(q)
+    for i in range(len(q)-1):
+        if q[i]!=q[i+1]:
+            continue
+        if q[i] == q[i+1]:
+            q.popleft()
+            q.popleft()
+            j -= 2
+            break
+    if before_q == len(q):
+        break
+    if len(q)==0:
+        print (q)
+        break
+    print(q)
+    j -= 1
+'''
+def solution(s):
+    stack = []
+    #stack.append(s[0])
+    j=0
+    for i in range(0,len(s)):
+        if len(stack) == 0:
+            stack.append(s[i])
+        else:
+            if stack[j] != s[i]:
+                stack.append(s[i])
+                j+=1
+            elif stack[j] == s[i]:
+                stack.pop()
+                j-=1
+                if len(stack) == 0:
+                    j = 0
+        print(stack)
+        print(j)
+    if len(stack) == 0:
+        return 1
+    else:
+        return 0
 
-print(q)
+s = "baabaa"
+print(solution(s))
