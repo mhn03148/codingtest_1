4fbb18cd54128c7c833e36f5061f62b6ccaf30a2
jujinhyeong <mhn03148@naver.com>
2022-11-05 12:22:58 +0900

    대각선 이동

--- python_useful_skill/find_road.py
+++ python_useful_skill/find_road.py
@@ -1,1 +1,9 @@
 worldmap = ["..XXX","..XXX","...XX","X....","XXX.."]
+n = len(worldmap)
+m = len(worldmap[0])
+for i in range(len(worldmap)):
+    worldmap[i] += "X"
+worldmap.append("X"*len(worldmap[0]))
+while True:
+
+print(worldmap)
