1f21549fa0bf7cc83bc1e915b7bba4214ba9dc44
jujinhyeong <mhn03148@naver.com>
2022-11-06 21:37:17 +0900

    대각선 이동.

--- python_useful_skill/find_road.py
+++ python_useful_skill/find_road.py
@@ -1,9 +1,18 @@
 worldmap = ["..XXX","..XXX","...XX","X....","XXX.."]
 n = len(worldmap)
 m = len(worldmap[0])
+count = 0
+x, y =0, 0
 for i in range(len(worldmap)):
     worldmap[i] += "X"
 worldmap.append("X"*len(worldmap[0]))
 while True:
-
+    if x==n-1and y==m-1:
+        break
+    if worldmap[x+1][y] == "." and worldmap[x][y+1] == "." and worldmap[x+1][y+1] == "." :
+        x+=1
+        y+=1
+        count+=1
+        continue
+    if worldmap[x+1][y] == "." and worldmap[x][y+1] == "." and worldmap[x+1][y+1] == ".":
 print(worldmap)
