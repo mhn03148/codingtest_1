68e2b2a3a2d86b013863e70ebbff920c00d03b5b
jujinhyeong <mhn03148@naver.com>
2023-04-25 11:31:08 +0900

    dfs와bfs를 이용하여 노드에 방문한 순서 출력

--- DFS_BFS/boj_q1260DfsandBfs.py
+++ DFS_BFS/boj_q1260DfsandBfs.py
@@ -1,4 +1,3 @@
-
 from collections import deque
 n ,m, v = map(int,input().split())
 q = []
@@ -38,49 +37,3 @@ def bfs(graph2,v,visited_b):
 dfs(graph2,v,visited)
 print(end="\n")
 bfs(graph2,v,visited_b)
-'''
-def DFS(v):
-    visited[v]=1
-    dfs.append(v)
-    for i in node[v]:
-        if (visited[i]==0):
-            DFS(i)
-
-def BFS(v):
-    visited[v]=1
-    bfs.append(v)
-    queue = [v]
-
-    while(queue):
-        for i in node[queue.pop(0)]:
-            if(visited[i]==0):
-                visited[i]=1
-                bfs.append(i)
-                queue.append(i)
-##################MAIN##################
-N, M, V = map(int, input().split())
-
-node =[[]for _ in range(N+1)]
-visited = [0]*(N+1)
-dfs = []
-bfs = []
-
-for i in range(M):
-    a, b = map(int, input().split())
-    node[a].append(b)
-    node[b].append(a)
-
-for j in range(N+1):
-    node[j].sort()
-
-DFS(V)
-for j in range(N+1):
-    visited[j]=0
-BFS(V)
-
-for m in dfs:
-    print(m, end=' ')
-print()
-for n in bfs:
-    print(n, end=' ')
-'''
