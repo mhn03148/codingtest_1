7e1a329fa27aff6bed5d66e921c56668bc82a100
jujinhyeong <mhn03148@naver.com>
2023-04-26 13:49:50 +0900

    N×M크기의 배열로 표현되는 미로가 있다.
    1	0	1	1	1	1
    1	0	1	0	1	0
    1	0	1	0	1	1
    1	1	1	0	1	1
    미로에서 1은 이동할 수 있는 칸을 나타내고, 0은 이동할 수 없는 칸을 나타낸다. 이러한 미로가 주어졌을 때, (1, 1)에서 출발하여 (N, M)의 위치로 이동할 때 지나야 하는 최소의 칸 수를 구하는 프로그램을 작성하시오. 한 칸에서 다른 칸으로 이동할 때, 서로 인접한 칸으로만 이동할 수 있다.
    
    위의 예에서는 15칸을 지나야 (N, M)의 위치로 이동할 수 있다. 칸을 셀 때에는 시작 위치와 도착 위치도 포함한다.

--- graph/boj_q2178Maze.py
+++ graph/boj_q2178Maze.py
@@ -1,32 +1,22 @@
 from collections import deque
-N,M = map(int,input().split())
-maze = [[0] * M for _ in range(N)]
-distance = 0
+n, m = map(int,input().split())
+maze = []
+for i in range(n):
+    maze.append(list(map(int,input())))
 
-for i in range(N):
-    s = input()
-    for j in range(M):
-        maze[i][j]=(int(s[j]))
 
-print(maze)
-
-dx = [-1,1,0,0]
-dy = [0,0,1,-1]
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
 
 q = deque()
 q.append((0,0))
-print(maze[0][0])
+
 while q:
-    x, y = q.popleft()
+    x,y = q.popleft()
     for i in range(4):
-        nx = x+dx[i]
-        ny = y+dy[i]
-        if 0<= nx < N and 0<= ny < M  and maze[nx][ny] == 1:
-            print(nx,ny)
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0<=nx<n and 0<=ny<m and maze[nx][ny] == 1:
+            maze[nx][ny] = maze[x][y]+1
             q.append((nx,ny))
-            print(q)
-            maze[nx][ny]=maze[x][y]+1
-
-
-print(maze)
-print(maze[N-1][M-1])
+print(maze[n-1][m-1])
