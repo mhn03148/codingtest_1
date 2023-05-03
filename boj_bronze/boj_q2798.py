8b315119b0e38f799aaf8cef81f4c974e02cd9f2
jujinhyeong <mhn03148@naver.com>
2023-05-03 11:15:40 +0900

    .

---
+++ boj_bronze/boj_q2798.py
@@ -0,0 +1,14 @@
+N, M = map(int,input().split())
+card = list(map(int, input().split()))
+ans = 0
+
+
+for i in range(N):
+    for j in range(i+1, N):
+        for k in range (j+1,N):
+            if card[i] + card[j] + card[k] > M:
+                continue
+            else:
+                ans = max(ans,card[i] + card[j] + card[k] )
+
+print(ans)
