d412c4cde5fa1f62e1b7722d46fce8244d5963b1
jujinhyeong <mhn03148@naver.com>
2023-06-01 21:01:31 +0900

    .

--- boj_silver/boj_q11866.py
+++ boj_silver/boj_q11866.py
@@ -4,14 +4,15 @@ arr=[]
 ans = []
 for i in range(N):
     arr.append(i+1)
-
+#arr = [1,2,3,4,5,6,7]
 while arr:
     for i in range(K-1):
-        arr.append(arr.pop(0))
-    ans.append(arr.pop(0))
+        arr.append(arr.pop(0)) #맨 앞부터 차례대로 배열의 뒤로 보냄
+    ans.append(arr.pop(0))# 해당하는 배열의 0번째 값을 정답 배열에 추가하고, 원래 배열에서 삭제 시킴
 
 s = str(ans)
 s = s[1:]
 s = s[:-1]
 s = "<" + s + ">"
 print(s)
+
