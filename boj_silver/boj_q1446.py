cb555c910f75dc4bce0b17b02eb053e5852bee07
jujinhyeong <mhn03148@naver.com>
2023-06-04 22:59:02 +0900

    .

--- boj_silver/boj_q1446.py
+++ boj_silver/boj_q1446.py
@@ -2,5 +2,5 @@ N, D = map(int,input().split())
 Sroad = []
 for _ in range(N):
     a, b, c = map(int,input().split())
-    Sroad.append(list(map(int,input().split())))
+    Sroad.append( list(map(int,input().split())))
 
