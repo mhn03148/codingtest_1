d4c538a85701b32967a15ba9c2236b77e6728a55
jujinhyeong <mhn03148@naver.com>
2023-07-01 21:00:18 +0900

    .

--- boj_silver/boj_q1325.py
+++ boj_silver/boj_q1325.py
@@ -1,8 +1,26 @@
 N, M = map(int, input().split(" "))
 comArray = []
+comHacMap = [0 for i in range(N)]
+comDict = {}
+
 for _ in range(M):
     firstNum, secondNum = map(int, input().split(" "))
     comArray.append([firstNum, secondNum])
 
+for i in range(1,N+1):
+    comDict[i] = []
+
+for i in range(len(comArray)):
+     comDict[comArray[i][0]].append(comArray[i][1])
+
+print(comDict)
 print(comArray)
+
+for i in comDict:
+    for j in comDict[i]:
+        print(i, comDict[j])
+    #print(comDict[i])
+
+
+print(comHacMap)
 
