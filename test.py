70fbc2bbde5e947fe5b7f022be22fc57f4cac0fa
jujinhyeong <mhn03148@naver.com>
2023-07-09 21:47:59 +0900

    .

--- test.py
+++ test.py
@@ -1,14 +1,5 @@
-n = int(input())
-password = []
-for i in range(n):
-    password.append(input())
-
-for i in range(len(password)):
-    if len(password[i]) >= 6 and len(password[i]) < 10 :
-        password[i] = "yes"
-    else:
-        password[i] = "no"
-
-for i in password:
-    print(i,end="\n")
+a = [[1,2],[3,4]]
+b = [[1,2]]
+if b[0] in a:
+    print("!")
 
